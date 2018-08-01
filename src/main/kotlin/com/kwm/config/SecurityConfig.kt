14ddc6d833b1659ba7b20b15b5ad52ff942a46b8
Keuller Magalhaes <keuller.magalhaes@gmail.com>
2018-08-01 01:42:59 -0300

    Security config bean.

---
+++ src/main/kotlin/com/kwm/config/SecurityConfig.kt
@@ -0,0 +1,24 @@
+package com.kwm.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+open class SecurityConfig : WebSecurityConfigurerAdapter {
+
+    constructor() {}
+
+    constructor(disableDefaults: Boolean = true) : super(disableDefaults)
+
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+            .antMatchers("/").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().permitAll()
+            .and()
+            .logout().permitAll()
+    }
+
+}
